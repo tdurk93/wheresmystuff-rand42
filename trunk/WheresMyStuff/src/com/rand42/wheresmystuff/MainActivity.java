3125ed781ddb2d4673e396e64af528ed81db235c
alexbettadapur@gmail.com <alexbettadapur@gmail.com@9e5215d2-54b2-0063-b3b0-fedc8595a430>
2013-02-14 01:28:58 +0000

    Login Page created, button listener added

--- trunk/WheresMyStuff/src/com/rand42/wheresmystuff/MainActivity.java
+++ trunk/WheresMyStuff/src/com/rand42/wheresmystuff/MainActivity.java
@@ -2,7 +2,9 @@ package com.rand42.wheresmystuff;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void submitLogin(View view)
+	{
+		Log.i("MainActivity","Clicked");
 	}
 
 }
