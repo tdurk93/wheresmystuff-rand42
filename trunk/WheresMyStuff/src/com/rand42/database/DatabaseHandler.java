7abb2a6a509e53c5f4bd7c614e5aa0342fba8c12
alexbettadapur@gmail.com <alexbettadapur@gmail.com@9e5215d2-54b2-0063-b3b0-fedc8595a430>
2013-02-14 05:26:13 +0000

    Basic login template created. DatabaseHandler created. Nothing works here

---
+++ trunk/WheresMyStuff/src/com/rand42/database/DatabaseHandler.java
@@ -0,0 +1,65 @@
+package com.rand42.database;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseUser;
+import com.parse.ParseException;
+import com.parse.SignUpCallback;
+
+public class DatabaseHandler 
+{
+	private static DatabaseHandler handler;
+	
+	private DatabaseHandler()
+	{
+		
+	}
+	public static DatabaseHandler getHandler()
+	{
+		if(handler==null)
+			handler = new DatabaseHandler();
+		return handler;
+	}
+	
+	public static boolean login(String email, String password)
+	{
+		boolean login=false;
+		ParseUser.logInInBackground(email,password, new LogInCallback()
+		{
+			public void done(ParseUser user, ParseException e)
+			{
+				if(user!=null)
+				{}
+					//success
+				else
+				{
+					//fail, check exception for details
+				}
+			}
+		});
+		return login;
+	}
+	
+	public static boolean createUser(String email, String password)
+	{
+		ParseUser user = new ParseUser();
+		user.setUsername(email);
+		user.setPassword(password);
+		
+		user.signUpInBackground(new SignUpCallback() 
+		{
+			public void done(ParseException e)
+			{
+				if(e==null)
+				{
+					//success
+				}
+				else
+				{
+					//fail. details in e
+				}
+			}
+		});
+		return true;
+	}
+}
