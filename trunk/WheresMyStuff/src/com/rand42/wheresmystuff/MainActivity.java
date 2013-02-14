02d83254e3f5100216f4340d2cfe33f27b8a74f6
alexbettadapur@gmail.com <alexbettadapur@gmail.com@9e5215d2-54b2-0063-b3b0-fedc8595a430>
2013-02-14 06:08:12 +0000

    Login verification working

--- trunk/WheresMyStuff/src/com/rand42/wheresmystuff/MainActivity.java
+++ trunk/WheresMyStuff/src/com/rand42/wheresmystuff/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rand42.wheresmystuff;
 
+import com.parse.ParseUser;
 import com.rand42.database.DatabaseHandler;
 
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -25,8 +27,15 @@ public class MainActivity extends Activity {
 	
 	public void submitLogin(View view)
 	{
+		//Working properly. According to design standards? Needs data validation and better notification
 		DatabaseHandler db = DatabaseHandler.getHandler();
-		db.createUser("alexbettadapur@gmail.com", "biscuit1");
+		EditText emailField = (EditText)findViewById(R.id.emailField);
+		EditText passwordField = (EditText)findViewById(R.id.passwordField);
+		db.login(emailField.getText().toString(), passwordField.getText().toString());
+		if(ParseUser.getCurrentUser()!=null)
+		{
+			Log.i("MainActivity", ParseUser.getCurrentUser().getUsername());
+		}
 		Log.i("MainActivity","Clicked");
 	}
 
