d7f13d4b6d53e4e44b3a99079a19a8de102b97af
alexbettadapur@gmail.com <alexbettadapur@gmail.com@9e5215d2-54b2-0063-b3b0-fedc8595a430>
2013-03-02 01:34:18 +0000

    Created new activities

---
+++ trunk/WheresMyStuff/src/com/rand42/wheresmystuff/NewItemActivity.java
@@ -0,0 +1,20 @@
+package com.rand42.wheresmystuff;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 3/1/13
+ * Time: 8:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewItemActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item);
+    }
+}
---
+++ trunk/WheresMyStuff/src/com/rand42/wheresmystuff/ViewItemActivity.java
@@ -0,0 +1,19 @@
+package com.rand42.wheresmystuff;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 3/1/13
+ * Time: 8:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ViewItemActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
