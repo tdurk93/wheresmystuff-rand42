e9c40e55d41b5b254b081681afea12a1beda6b05
alexbettadapur@gmail.com <alexbettadapur@gmail.com@9e5215d2-54b2-0063-b3b0-fedc8595a430>
2013-03-07 00:57:48 +0000

    Files werent added

---
+++ trunk/WheresMyStuff/src/com/rand42/presenters/HomePresenter.java
@@ -0,0 +1,37 @@
+package com.rand42.presenters;
+
+import com.rand42.model.IModel;
+import com.rand42.model.LocalModel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 3/6/13
+ * Time: 5:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HomePresenter
+{
+    private IModel model;
+    public HomePresenter()
+    {
+        model = LocalModel.getModel();
+    }
+
+    /**
+     * Logs out the user
+     */
+    public void logOut()
+    {
+      model.logOut();
+    }
+
+    /**
+     * Gets the name of the current user
+     * @return
+     */
+    public String getUserName()
+    {
+        return model.getUser().getName();
+    }
+}
