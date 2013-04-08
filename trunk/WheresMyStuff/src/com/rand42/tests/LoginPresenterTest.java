44cc450c12f94ed8b9a21ebbc6c145ad4157fe14
alexbettadapur@gmail.com <alexbettadapur@gmail.com@9e5215d2-54b2-0063-b3b0-fedc8595a430>
2013-04-08 16:19:28 +0000

    Test package. Place your tests here

---
+++ trunk/WheresMyStuff/src/com/rand42/tests/LoginPresenterTest.java
@@ -0,0 +1,19 @@
+package com.rand42.tests;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 4/7/13
+ * Time: 8:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginPresenterTest
+{
+    @Test(timeout=200)
+    public void testA()
+    {
+
+    }
+}
