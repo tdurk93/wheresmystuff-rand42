bf775537cc1bd5b0630bb585640aacca38b1525b
nolanhcktt <nolanhcktt@gmail.com>
2013-04-09 03:51:26 +0000

    These are being committed by a confused kitty kat who fails at keeping her subversion from dying.

--- WheresMyStuff/src/com/rand42/presenters/ViewUserPresenter.java
+++ WheresMyStuff/src/com/rand42/presenters/ViewUserPresenter.java
@@ -17,28 +17,53 @@ public class ViewUserPresenter
     private final IModel model;
     private final IViewUserView view;
 
+    /**
+     * Default constructor for view user presented
+     * @param model
+     * @param view
+     */
     public ViewUserPresenter(IModel model, IViewUserView view)
     {
         this.model = model;
         this.view = view;
     }
 
+    /**
+     * Returns a user given the user's id
+     * @param the user's id number
+     * @return that user
+     */
     public User loadUser(long id)
     {
         return model.getUser(id);
     }
 
 
+    /**
+     * Locks a user account
+     * @param user account to lock
+     */
     public void lockUser(User currentUser)
     {
         model.lockUser(currentUser);
 
     }
+    
+    /**
+     * unlocks a user account
+     * @param user account to unlock
+     */
     public void unlockUser(User user)
     {
         model.unlockUser(user);
     }
 
+    
+    /**
+     * Deletes a user
+     * @param user to delete
+     * @return whether or not user was deleted
+     */
     public boolean deleteUser(User currentUser)
     {
 
